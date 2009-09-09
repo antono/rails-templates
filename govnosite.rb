12cbf42b0c10857b7e06e89b8f8e22ca2595de55
Antono Vasiljev <antono.vasiljev@gmail.com>
2009-09-09 21:21:57 +0300

    Conditions for some plugin vendoring

--- govnosite.rb
+++ govnosite.rb
@@ -10,7 +10,7 @@ run "echo TODO > README"
 run "rm doc/README_FOR_APP"
 run "rm public/index.html"
 run "rm public/favicon.ico"
-run "rm public/rails.png"
+run "rm public/images/rails.png"
 file 'public/robots.txt', "User-Agent: *\nAllow: /"
 
 ##
@@ -22,11 +22,13 @@ git :init
 # Dirs should contain at least one file to be tracked by git
 run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitignore \\;")
 
+
 # Creating global .gitignore
 # Ignoring swaps, docs, logs and schema.rb
 # Migrations is the only thru about db schema, not schema.rb!
 #
-file '.gitignore', "log/\\*.log
+file '.gitignore', <<-EOF
+log/\\*.log
 log/\\*.pid
 db/\\*.db
 db/\\*.sqlite3
@@ -39,7 +41,7 @@ config/database.yml
 *~
 *.swp
 .DS_Store
-"
+EOF
 
 # Only database.yml.example should be stored in git for security reasons!
 run "cp config/database.yml config/database.yml.example"
@@ -47,19 +49,32 @@ run "cp config/database.yml config/database.yml.example"
 git :add => "."
 git :commit => "-a -m 'Setting up a new rails app. Copy config/database.yml.example to config/database.yml and customize.'"
 
+### 
+### We need some info about ongoing project
+###
+puts 'Some questions about ongoing project:'
+uses_jquery = yes('Uses jQuery?')
+uses_exception_notification = yes('Need exception_notifier?')
+uses_aasm = yes('Need finite state machines?')
+uses_asset_packager = yes('Need asset packaging?')
+
 ###
 ### Let's setup gems and plugin dependencies
 ###
 
 # Rails plugins as git submodules
-plugin 'exception_notifier', :git => 'git://github.com/rails/exception_notification.git', :submodule => true
-plugin 'asset_packager', :git => 'git://github.com/sbecker/asset_packager.git', :submodule => true
-plugin 'authlogic', :git => 'git://github.com/binarylogic/authlogic.git', :submodule => true
-plugin 'will_paginate', :git => 'git://github.com/mislav/will_paginate.git', :submodule => true
-plugin 'jrails', :git => 'git://github.com/aaronchi/jrails.git', :submodule => true
+
+# Always use it:
+plugin('will_paginate', :git => 'git://github.com/mislav/will_paginate.git', :submodule => true)
+
+# Customizable
+plugin('asset_packager', :git => 'git://github.com/sbecker/asset_packager.git', :submodule => true) if uses_asset_packager
+plugin('jrails', :git => 'git://github.com/aaronchi/jrails.git', :submodule => true) if uses_jquery
+plugin('exception_notifier', :git => 'git://github.com/rails/exception_notification.git', :submodule => true) if uses_exception_notification
+
 
 # Some useful gems
-gem 'rubyist-aasm'
+# gem 'rubyist-aasm'
 
 # Freezing gems
 rake("gems:install", :sudo => true)
