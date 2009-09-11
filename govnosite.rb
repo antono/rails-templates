b405b4533ed48a676a277a192e0193b562130833
Antono Vasiljev <antono.vasiljev@gmail.com>
2009-09-11 19:41:45 +0300

    refactoring + cucumber

--- govnosite.rb
+++ govnosite.rb
@@ -1,19 +1,36 @@
 ### -*- coding: utf-8 -*-
 ###
-### Govnosite Template for Rails
+### Govnosite Rails Template
 ###
 
-##
-## Initial files setup
-##
+def commit_all_with_message(message)
+  git :add => ".", :commit => "-a -m #{message}"
+end
 
+puts 'Some questions about ongoing project:'
+
+need_jquery = yes?('Uses jQuery?')
+need_aasm = yes?('Need finite state machines?')
+need_asset_packager = yes?('Need asset packaging?')
+need_exception_notification = yes?('Need exception_notifier?')
+need_attachements = yes?('Need file or image uploads?')
+need_wysiwyg = yes?('Need WYSIWYG editor?')
+need_russian = yes?('Need russian gem?')
+
+need_authorisation = yes?('Need authorisation support?')
+if need_authorisation
+  need_roles = yes?('Need user roles?')
+end
+
+##
+## Initial git setup
+##
 run "echo TODO > README"
 run "rm doc/README_FOR_APP"
 run "rm public/index.html"
 run "rm public/favicon.ico"
 run "rm public/images/rails.png"
 file 'public/robots.txt', "User-Agent: *\nAllow: /"
-
 
 git :init
 
@@ -35,25 +52,9 @@ config/database.yml
 .DS_Store
 EOF
 
-run "cp config/database.yml config/database.yml.example"
+commit_all_with_message('Basic setup of rails app. .gitignore, robots.txt and other stuff.')
 
-git :add => "."
-git :commit => "-a -m 'Setting up a new rails app. Copy config/database.yml.example to config/database.yml and customize.'"
-
-puts 'Some questions about ongoing project:'
-need_jquery = yes?('Uses jQuery?')
-need_aasm = yes?('Need finite state machines?')
-need_asset_packager = yes?('Need asset packaging?')
-need_exception_notification = yes?('Need exception_notifier?')
-need_attachements = yes?('Need file or image uploads?')
-need_wysiwyg = yes?('Need WYSIWYG editor?')
-need_russian = yes?('Need russian gem?')
-
-need_authorisation = yes?('Need authorisation support?')
-if need_authorisation
-  need_roles = yes?('Need user roles?')
-end
-
+# Plugins and gems               
 plugin('will_paginate', :git => 'git://github.com/mislav/will_paginate.git', :submodule => true)
 plugin('asset_packager', :git => 'git://github.com/sbecker/asset_packager.git', :submodule => true) if need_asset_packager
 plugin('jrails', :git => 'git://github.com/aaronchi/jrails.git', :submodule => true) if need_jquery
@@ -72,17 +73,32 @@ if need_aasm
 end
 
 gem 'ryanb-nifty-generators', :source => 'http://gems.github.com'
+gem 'cucumber'
 
 rake("gems:install", :sudo => true)
 rake("gems:unpack")
 
-git :submodule => "init", :add => '.', :commit => "-a -m 'Useful plugins as submodules and unpacked gems'"
+git :submodule => "init" 
+
+commit_all_with_message 'Useful plugins as submodules and unpacked gems'
+
+
+# Generating stuff
+run 'capify .'
+commit_all_with_message 'Capified'
 
 generate :nifty_layout
+commit_all_with_message 'Nifty layout'
+
 generate :nifty_config
+commit_all_with_message 'Nifty config'
+
+generate :cucumber
+commit_all_with_message 'Cucumbered'
 
 if need_authorisation
   generate("authlogic", "user session")
+  commit_all_with_message 'Authlogic base models'
 end
 
 rake("db:migrate")
