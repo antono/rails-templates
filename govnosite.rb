a989e7042bff5aaf2c5e2d6a047460a2c2e2748c
Antono Vasiljev <antono.vasiljev@gmail.com>
2009-09-11 19:27:05 +0300

    fixes

--- govnosite.rb
+++ govnosite.rb
@@ -6,6 +6,7 @@
 ##
 ## Initial files setup
 ##
+
 run "echo TODO > README"
 run "rm doc/README_FOR_APP"
 run "rm public/index.html"
@@ -13,20 +14,11 @@ run "rm public/favicon.ico"
 run "rm public/images/rails.png"
 file 'public/robots.txt', "User-Agent: *\nAllow: /"
 
-##
-## Git initialization and first commit
-##
 
 git :init
 
-# Dirs should contain at least one file to be tracked by git
 run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitignore \\;")
 
-
-# Creating global .gitignore
-# Ignoring swaps, docs, logs and schema.rb
-# Migrations is the only thru about db schema, not schema.rb!
-#
 file '.gitignore', <<-EOF
 log/\\*.log
 log/\\*.pid
@@ -43,52 +35,54 @@ config/database.yml
 .DS_Store
 EOF
 
-# Only database.yml.example should be stored in git for security reasons!
 run "cp config/database.yml config/database.yml.example"
 
 git :add => "."
 git :commit => "-a -m 'Setting up a new rails app. Copy config/database.yml.example to config/database.yml and customize.'"
 
-### 
-### We need some info about ongoing project
-###
 puts 'Some questions about ongoing project:'
-uses_jquery = yes('Uses jQuery?')
-uses_exception_notification = yes('Need exception_notifier?')
-uses_aasm = yes('Need finite state machines?')
-uses_asset_packager = yes('Need asset packaging?')
+need_jquery = yes?('Uses jQuery?')
+need_aasm = yes?('Need finite state machines?')
+need_asset_packager = yes?('Need asset packaging?')
+need_exception_notification = yes?('Need exception_notifier?')
+need_attachements = yes?('Need file or image uploads?')
+need_wysiwyg = yes?('Need WYSIWYG editor?')
+need_russian = yes?('Need russian gem?')
 
-###
-### Let's setup gems and plugin dependencies
-###
+need_authorisation = yes?('Need authorisation support?')
+if need_authorisation
+  need_roles = yes?('Need user roles?')
+end
 
-# Rails plugins as git submodules
-
-# Always use it:
 plugin('will_paginate', :git => 'git://github.com/mislav/will_paginate.git', :submodule => true)
+plugin('asset_packager', :git => 'git://github.com/sbecker/asset_packager.git', :submodule => true) if need_asset_packager
+plugin('jrails', :git => 'git://github.com/aaronchi/jrails.git', :submodule => true) if need_jquery
+plugin('exception_notifier', :git => 'git://github.com/rails/exception_notification.git', :submodule => true) if need_exception_notification
+plugin('paperclip', :git => 'git://github.com/thoughtbot/paperclip.git', :submodule => true) if need_attachements
+plugin('tiny_mce', :git => 'git://github.com/kete/tiny_mce.git', :submodule => true) if need_wysiwyg
+plugin('russian', :git => 'git://github.com/yaroslav/russian.git', :submodule => true) if need_russian
 
-# Customizable
-plugin('asset_packager', :git => 'git://github.com/sbecker/asset_packager.git', :submodule => true) if uses_asset_packager
-plugin('jrails', :git => 'git://github.com/aaronchi/jrails.git', :submodule => true) if uses_jquery
-plugin('exception_notifier', :git => 'git://github.com/rails/exception_notification.git', :submodule => true) if uses_exception_notification
+if need_authorisation
+  plugin('authlogic', :git => 'git://github.com/binarylogic/authlogic.git', :submodule => true)
+  plugin('acl9', :git => 'git://github.com/be9/acl9.git', :submodule => true) if need_roles
+end
 
+if need_aasm
+  gem 'rubyist-aasm', :source => 'http://gems.github.com'
+end
 
-# Some useful gems
-# gem 'rubyist-aasm'
+gem 'ryanb-nifty-generators', :source => 'http://gems.github.com'
 
-# Freezing gems
 rake("gems:install", :sudo => true)
 rake("gems:unpack")
 
 git :submodule => "init", :add => '.', :commit => "-a -m 'Useful plugins as submodules and unpacked gems'"
 
-###
-### Generating base migrations and other stuff
-###
+generate :nifty_layout
+generate :nifty_config
 
-generate("authlogic", "user session")
+if need_authorisation
+  generate("authlogic", "user session")
+end
 
-###
-### DB: migration, population
-###
 rake("db:migrate")
